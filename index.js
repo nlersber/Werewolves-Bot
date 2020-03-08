fa62251e21d847d484815fd95c272eda3b9dc2e1
nlersber <nick.lersberghe@student.hogent.be>
2020-03-09 00:04:47 +0100

    Added node packages. Added preliminary js code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Test')
+const Discord = require("discord.js"); //Load discord library
+const bot = new Discord.Client(); //Loading bot itself
+
+bot.on("message", message => {
+  if (message.content == "ping") message.channel.send("pong");
+});
+
+bot.login("Njg2MzQ0MzIyMDg5NjE1Mzg3.XmV29w.3886qeyRxJrnWmvH9cTngyq6jw4");
