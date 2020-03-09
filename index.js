1089ef2e832b799578ecf0fb4381e48de51e41a1
nlersber <nick.lersberghe@student.hogent.be>
2020-03-09 23:10:43 +0100

    Fixed node packages that prevented working bot

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const Discord = require("discord.js");
 const settings = require("./settings.json");
+const path = require("path");
+const commando = require("discord.js-commando");
 
-const bot = new Discord.Client({disableEveryone: true});
+const client = new commando.CommandoClient({
+  owner: "686344322089615387", // Your ID here.
+  commandPrefix: "$", // The prefix of your bot.
+  unknownCommandResponse: false // Set this to true if you want to send a message when a user uses the prefix not followed by a command
+});
 
-bot.login(settings.token)
+client.on('ready', () => {
+    console.log('Yup');
+})
+
+
+client.registry.registerDefaults();
+
+client.login(settings.token);
